0e28b625f0950adcbc72fad38ce1ec3ef16bd62f
roy759 <anikr4277@gmail.com>
2025-04-15 20:57:32 +0530

    Updated external service URL

--- src/services/external.js
+++ src/services/external.js
@@ -2,13 +2,10 @@ const axios = require('axios');
 
 async function callExternalService() {
   try {
-    const response = await axios.get(process.env.N8N_WEBHOOK_URL);
+    const response = await axios.get('https://webhook.site/e3d1ab14-c68c-4282-856e-13fe2c86e444');
     return response.data;
   } catch (error) {
-    return {
-      error: 'External service call failed',
-      message: error.message,
-    };
+    return { error: 'External service call failed', message: error.message };
   }
 }
 
